c69a98ef8bd824b4670f6ebd36fbb544ce392018
veropesant <pesant.vero@gmail.com>
2018-10-29 13:37:06 -0400

    Ball.js line 56 : added the ',' between the two values in parentheses

--- exercises/exercise6/js/Ball.js
+++ exercises/exercise6/js/Ball.js
@@ -53,7 +53,7 @@ Ball.prototype.isOffScreen = function () {
 //
 // Draw the ball as a rectangle on the screen
 Ball.prototype.display = function () {
-  rect(this.x this.y);
+  rect(this.x,this.y);
 }
 
 // handleCollision(paddle)
