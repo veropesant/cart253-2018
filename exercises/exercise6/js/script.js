6a86fb29be23d9b0c883c61de5809c7c096cc461
veropesant <pesant.vero@gmail.com>
2018-10-29 13:21:13 -0400

    Paddle.js line 29 : closed the parentheses

--- exercises/exercise6/js/script.js
+++ exercises/exercise6/js/script.js
@@ -26,7 +26,7 @@ function setup() {
   rightPaddle = new Paddle(width-10,height/2,10,600,10,UP_ARROW,DOWN_ARROW);
   // Create the left paddle with W and S as controls
   // Keycodes 83 and 87 are W and S respectively
-  leftPaddle = new Paddle(0,height/2,10,60,10,83,87;
+  leftPaddle = new Paddle(0,height/2,10,60,10,83,87); 
 
 
 // draw()
