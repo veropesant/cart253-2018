9356c5ae767590f4ba8cd30f157bd71ebcd8c7e1
Véronique Pesant <pesant.vero@gmail.com>
2018-11-13 16:51:12 -0500

    uploaded exercise 7
    It contains: my index, my script, my class Particle, the librairy p5.dom.js and p5.sound.min.js

---
+++ exercises/exercise7/js/script.js
@@ -0,0 +1,49 @@
+/*****************
+
+Experiment with camera
+Véronique Pesant
+
+Eventually my goal is to create a drawing tool using the webcam.
+It would preferably be used in front of a white background to make the
+colors pop out more.
+I would like to scan the pixels on the webcam and spot for example which
+pixels are bright red, and then track these pixels and create a line as
+the user moves the object around, using this same color,
+allowing the user to draw.
+
+In this first experiment, I simply try to use the webcam and spot the colors
+of the pixels, because that's something I've never worked with previously.
+
+******************/
+
+//Variables
+var capture;
+var capScale = 16;
+var pixels = [];
+var slider;
+var maxPixels=50;
+
+// setup()
+//
+// Description of setup
+
+function setup() {
+  createCanvas(640,480);
+  pixelDensity(1);
+  capture = createCapture(VIDEO);
+  capture.size(width/capScale, height/capScale);
+  for(var i=0; i<maxPixels; i++){
+    pixels[i] = new Particle(random(width), random(height));
+  }
+  slider = createSlider(0, 255, 127);
+  background(0);
+}
+
+function draw(){
+  capture.loadPixels();
+  for(var i=0; i<pixels.length; i++){
+    pixels[i].update();
+    pixels[i].show();
+  }
+
+}
